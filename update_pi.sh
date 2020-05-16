d3c8deb8f65c76bf59e67ad8337225ba866064c0
James Hawkins <jhawk154@gmail.com>
2020-05-16 14:26:17 -0400

    Add files via upload

---
+++ update_pi.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# script to update raspberry pi on startup
+# runs as startup via systemd
+
+echo "updating packages..."
+sudo apt-get update
+echo "upgrading packages..."
+sudo apt-get upgrade -y
+ts=$(date +"%Y-%m-%d %T")
+sudo echo "Last updated at: $ts" > /home/pirate/update/last_update.txt
+echo "fin.."
