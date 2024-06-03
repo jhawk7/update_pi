03125930cac5e653ef2821b6933650d47a3cd8d0
JHawk <13038876+jhawk7@users.noreply.github.com>
2024-06-03 19:58:05 -0400

    Update update_pi.sh
    output timestamp to stdout

--- update_pi.sh
+++ update_pi.sh
@@ -8,6 +8,7 @@ echo "upgrading packages..."
 sudo apt full-upgrade -y
 ts=$(date +"%Y-%m-%d %T")
 sudo echo "Last updated at: $ts" > /home/pi/update_pi/last_update.txt
+sudo echo "Last updated at: $ts"
 echo "fin.."
 #remove the lines below if running as service to avoid boot loop
 echo "rebooting.."
